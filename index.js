4782a5fa20fd6c6c9e7ff0f1be3354c8cfa68e96
Mateo Villa <creationsmateo@gmail.com>
2024-01-15 16:46:02 -0500

    extracting title

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const shortUrl = require('node-url-shortener');
 const mongoose = require('mongoose');
+
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
-const Urls = mongoose.model('Urls', { urlName: String, count: Number });
+const Urls = mongoose.model('Urls', {
+  urlName: String,
+  count: Number,
+  title: String,
+});
 
 const app = express();
 
@@ -12,12 +17,12 @@ const port = 3000;
 
 const insertUrl = async (url) => {
   const dbUrl = await Urls.find({ urlName: url });
-  if (dbUrl) {
+  console.log(dbUrl);
+  if (dbUrl.length > 0) {
     const updated = await Urls.updateOne(
       { urlName: url },
       { $inc: { count: 1 } }
     );
-    console.log(updated);
   } else {
     const newInsert = new Urls({ urlName: url });
     await newInsert.save();
@@ -38,11 +43,35 @@ app.post('/short', async (req, res) => {
 });
 
 app.get('/mostUsed', async (req, res) => {
-  // const urlTable = new Urls();
-
-  const data = await Urls.find().sort({ count: -1 }).limit(2);
+  const data = await Urls.find().sort({ count: -1 }).limit(100);
 
   return res.send(data);
+});
+
+const parseTitle = (body) => {
+  let match = body.match(/<title>([^<]*)<\/title>/); // regular expression to parse contents of the <title> tag
+  if (!match || typeof match[1] !== 'string')
+    throw new Error('Unable to parse the title tag');
+  return match[1];
+};
+
+app.get('/title', async (req, res) => {
+  // if (!url) return res.status(400).end('Missing url query parameter');
+
+  const url = 'https://cdpt.in/MzM4OTEyMg==';
+  const dbUrl = await Urls.findOne({ urlName: url });
+  const urlText = await fetch(url)
+    .then((res) => res.text()) // parse response's body as text
+    .then((body) => parseTitle(body)) // extract <title> from body
+    .catch((e) => res.status(500).end(e)); // catch possible errors
+  console.log(urlText);
+
+  const updated = await Urls.updateOne({ urlName: url }, { title: urlText });
+
+  console.log({ updated });
+  const data = await Urls.findOne({ urlName: url });
+
+  return res.send({ data });
 });
 
 app.listen(port, () => {
